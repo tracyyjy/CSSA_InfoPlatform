688b8e7e3652c0161de112e3053c2d7b14d9c9a6
tracyyjy <tracyyjy@gmail.com>
2013-10-25 16:57:44 -0700

    CS290B Porject Team 6 Scalable Internet Service
    Make UCSB CSSA GoogleGroup better efficient user-friendly

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CSSAInfoplatform::Application.config.secret_token = 'dc3bc65e5def670fd64c31414df185e93871ffce5ff2f5d0161d191a695ff2f6031cb87594a1b6fa7b63240cd38cf8d052ca68a3580f06f5b54db592f2aa6865'
