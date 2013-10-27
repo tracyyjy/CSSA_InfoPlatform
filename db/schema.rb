147915240fce10e0770922e94d3e2800ee633b84
tracyyjy <tracyyjy@gmail.com>
2013-10-27 10:59:17 -0700

    test local push pull

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20131027055741) do
 
   create_table "users", :force => true do |t|
@@ -20,6 +19,5 @@ ActiveRecord::Schema.define(:version => 20131027055741) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
-
 
 end
