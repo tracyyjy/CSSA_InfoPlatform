a28f394be958a6f0904973b7a5a8ed20a2a7baca
Mengyuan Gu <mgu@umail.ucsb.edu>
2013-11-07 20:19:58 -0800

    enable USER PUBLIC pills && create compose view

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,15 @@
+function setActive(thisId, className){
+  //alert(document.getElementById(thisId).className);
+  var nodeArr=document.getElementById("ul_mail_nav").getElementsByTagName("li");
+  //alert(nodeArr.length);
+  for (var i=0;i<nodeArr.length;i++){  	
+  	    //alert("here!");    
+        nodeArr[i].setAttribute("class",""); 
+        
+   }
+   
+  var obj=document.getElementById(thisId);  
+  obj.setAttribute("class",className);  
+  //alert(document.getElementById(thisId).className);
+  
+}
