28ea97627d2a8ec2c934be34e002d12e15d51e84
tracyyjy <tracyyjy@gmail.com>
2013-11-25 12:29:50 -0800

    bad edtion, just for httperf testing

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,18 +1,20 @@
 module SessionsHelper
   def sign_in(user)
-    cookies.permanent[:remember_token]=user.remember_token
+    # cookies.permanent[:remember_token]=user.remember_token
     self.current_user=user
   end
   
   def signed_in?
-    !current_user.nil?
+    true
+    # !current_user.nil?
   end  
   def current_user=(user)
     @current_user=user
   end
   
   def current_user
-    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+    # @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+     @current_user
   end
   
   def current_user?(user)
@@ -21,7 +23,7 @@ module SessionsHelper
   
   def sign_out
     self.current_user = nil
-    cookies.delete(:remember_token)
+    # cookies.delete(:remember_token)
     
   end
   
@@ -35,10 +37,11 @@ module SessionsHelper
   end
   
   def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_path, notice: "Please signed in."
-    end
+    true
+    # unless signed_in?
+#       store_location
+#       redirect_to signin_path, notice: "Please signed in."
+#     end
   end
   
 end
