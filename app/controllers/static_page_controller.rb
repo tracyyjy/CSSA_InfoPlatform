a3229fe7c5fba3dd8c1eeb982907ad6e7fa05abb
UCSBGauchos <byang@cs.ucsb.edu>
2013-12-08 01:38:12 -0800

    Cache for all post, all users and all groups

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,6 +1,15 @@
 class StaticPageController < ApplicationController
-
- def help
- end
+  caches_page :index
+  
+  def index
+    @users = User.all
+  end
+  
+  def help
+  end
+  
+  def create
+    expire_page :action => :index
+  end
 
 end
